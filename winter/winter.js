b6d108046a4ef95bfcd6323ed584f2d3e0814e11
Nakul Rawat <nakulrawat211@gmail.com>
2024-06-09 07:56:29 +0530

    skills-section

--- winter/winter.js
+++ winter/winter.js
@@ -16,7 +16,7 @@ tl.to(".winter_hero", {}, "a")
   .to(".winter_bottom-h1", { bottom: "60%" }, "a")
   .to(
     ".winter_hero .winter_content",
-    { top: "-30%", delay: -0.4, margin: "10% 0" },
+    { top: "-50%", delay: -0.4, margin: "0 0" },
     "b"
   )
   .from(
@@ -24,3 +24,35 @@ tl.to(".winter_hero", {}, "a")
     { y: "100%", scale: 0, opacity: 0 },
     "-=.4"
   );
+
+//image cover scaler
+
+(function () {
+  let chck_if_gsap_loaded = setInterval(function () {
+    const eleBuilder = document
+      .querySelector("body")
+      .classList.contains("elementor-editor-active");
+    const screenSize = window.screen.width >= 1025;
+    if (window.gsap && window.ScrollTrigger && !eleBuilder && screenSize) {
+      gsap.registerPlugin(ScrollTrigger);
+      image_mask();
+      clearInterval(chck_if_gsap_loaded);
+    }
+  }, 500);
+
+  function image_mask() {
+    const imageMask = document.querySelector(
+      ".elementor-element .circle-mask.elementor-widget-image .elementor-widget-container img"
+    );
+    gsap.to(imageMask, {
+      scrollTrigger: {
+        trigger: ".start-gsap-mask",
+        start: "-15% 25%",
+        end: "25% 25%",
+        scrub: 1,
+      },
+      webkitMaskSize: 70 + "%",
+      duration: 1,
+    });
+  }
+})();
