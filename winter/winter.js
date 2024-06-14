0ff674a98ccdcbea26112ba915eafab9f4e4a9a1
Nakul Rawat <nakulrawat211@gmail.com>
2024-06-14 10:38:00 +0530

    gsap scrolltrigger

--- winter/winter.js
+++ winter/winter.js
@@ -27,32 +27,42 @@ tl.to(".winter_hero", {}, "a")
 
 //image cover scaler
 
-(function () {
-  let chck_if_gsap_loaded = setInterval(function () {
-    const eleBuilder = document
-      .querySelector("body")
-      .classList.contains("elementor-editor-active");
-    const screenSize = window.screen.width >= 1025;
-    if (window.gsap && window.ScrollTrigger && !eleBuilder && screenSize) {
-      gsap.registerPlugin(ScrollTrigger);
-      image_mask();
-      clearInterval(chck_if_gsap_loaded);
-    }
-  }, 500);
+// (function () {
+//   let chck_if_gsap_loaded = setInterval(function () {
+//     // const eleBuilder = document
+//     //   .querySelector("body")
+//     //   .classList.contains("elementor-editor-active");
+//     const screenSize = window.screen.width >= 1025;
+//     if (window.gsap && window.ScrollTrigger && screenSize) {
+//       gsap.registerPlugin(ScrollTrigger);
+//       image_mask();
+//       clearInterval(chck_if_gsap_loaded);
+//     }
+//   }, 500);
 
-  function image_mask() {
-    const imageMask = document.querySelector(
-      ".elementor-element .circle-mask.elementor-widget-image .elementor-widget-container img"
-    );
-    gsap.to(imageMask, {
-      scrollTrigger: {
-        trigger: ".start-gsap-mask",
-        start: "-15% 25%",
-        end: "25% 25%",
-        scrub: 1,
-      },
-      webkitMaskSize: 70 + "%",
-      duration: 1,
-    });
-  }
-})();
+// function image_mask() {
+//   const imageMask = document.querySelector(".circle-mask img");
+//   gsap.to(imageMask, {
+//     scrollTrigger: {
+//       trigger: ".start-gsap-mask",
+//       start: "bottom bottom",
+//       end: "+=50vh",
+//       scrub: 1,
+//     },
+//     webkitMaskSize: 70 + "%",
+//     duration: 1,
+//   });
+// }
+// image_mask();
+gsap.registerPlugin(ScrollTrigger);
+gsap.to(".mask-container", {
+  scrollTrigger: {
+    trigger: ".mask-container",
+    start: "top bottom",
+    // markers: true,
+    scrub: true,
+    toggleAction: "restart none none pause",
+  },
+  webkitMaskSize: 80 + "%",
+  duration: 3,
+});
